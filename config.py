41382ce468e9df6ff58b85c1b612e17e4bfc5649
GeekyAnt <114788309+GeekyAnt@users.noreply.github.com>
2023-05-16 19:41:52 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+config = {
+    "openai_key": "[api-key]",
+    "database": {
+        "host": "[DB hostname or IP]",
+        "port": "5432",
+        "dbname": "[DatabaseName]",
+        "user": "[UserName]",
+        "password": "[Password]"
+    }
+}
